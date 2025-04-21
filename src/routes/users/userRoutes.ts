001ac8d66fd7a0ea9a93e9b15e2f9ad36c627698
lavitalle <vitorhugoinacio@outlook.com>
2025-04-21 14:35:33 -0300

    feat: User registration implemented.

--- src/routes/users/userRoutes.ts
+++ src/routes/users/userRoutes.ts
@@ -1,13 +1,23 @@
-import { Router, Request, Response} from 'express';
+import { Router, Request, Response } from "express";
+import UserService from "../../services/UserService";
 
 const usrRoute = Router();
+const userService = new UserService();
 
-usrRoute.get('/', (req: Request, res: Response) => {
+usrRoute.post("/create", (req: Request, res: Response) => {
+  try {
+    userService.userInsert(req, res);
+  } catch (error) {
+    res.status(500).json({ message: ("Error" + error) });
+  }
+});
+
+usrRoute.post("/login", (req: Request, res: Response) => {
     try {
-        res.send({ message: "Everything is okay."});
-    } catch (error) {
-        res.send({ message: "Some error occurs."});
-    }
+        res.send({ message: "okay, you're here." });
+      } catch (error) {
+        res.status(500).json({ message: ("Error" + error) });
+      } 
 });
 
 export default usrRoute;
